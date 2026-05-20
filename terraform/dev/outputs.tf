74e8f7e572c428489ce529146d80b1f15d119471
SunghoCha <61485641+SunghoCha@users.noreply.github.com>
2026-05-21 04:13:04 +0900

    dev DB 접근용 SSM 호스트 추가 (#31)

--- terraform/dev/outputs.tf
+++ terraform/dev/outputs.tf
@@ -49,6 +49,14 @@ output "rds_database_name" {
   value = aws_db_instance.main.db_name
 }
 
+output "db_access_instance_id" {
+  value = var.enable_db_access_host ? aws_instance.db_access_host[0].id : null
+}
+
+output "db_access_instance_private_ip" {
+  value = var.enable_db_access_host ? aws_instance.db_access_host[0].private_ip : null
+}
+
 output "raw_mail_bucket_name" {
   value = aws_s3_bucket.raw_mail.bucket
 }
